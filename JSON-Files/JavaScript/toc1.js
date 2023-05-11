82511632025a05bd21ab64537347f3a47c8358e9
Ahinojosa13 <128249514+Ahinojosa13@users.noreply.github.com>
2023-05-10 20:32:34 -0400

    Added new html, js

--- JSON-Files/JavaScript/toc1.js
+++ JSON-Files/JavaScript/toc1.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('./JSON-Files/accessoriesCard.json')
+  fetch('./JSON-Files/accessoriesCard.json')  //fetch request is made to retrieve a JSON file located at ./JSON-Files/accessoriesCard.json. //
     .then(response => response.json())
     .then(data => {
       const accessoriesCard = data.accessoriesCard;
       const jsonElement = document.getElementById('accessories-json'); 
       let html = '';
-
+// Then I create a for loop to go through my json file to input in a template for my product card
       accessoriesCard.forEach(accessoriesCard => {
         html += `
-          <div class="product-card">
-            <img src="${accessoriesCard.image}" alt="${accessoriesCard.alt}">
+          <div class="product-card">                                            
+            <img src="${accessoriesCard.image}" alt="${accessoriesCard.alt}"> 
             <h2>${accessoriesCard.name}</h2> 
             <p>${accessoriesCard.description}</p> 
             <span>${accessoriesCard.price}</span> 
@@ -17,9 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
           </div>
         `;
       });
-
+// Grab information and plugs in the HTML
       jsonElement.innerHTML = html;
     })
+//If there is any error it'll console so you can check over your code 
     .catch(error => {
       console.log('Error', error);
     });
